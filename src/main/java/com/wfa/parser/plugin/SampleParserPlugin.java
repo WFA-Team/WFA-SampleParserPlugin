1d6a3ea2537162afd6e920d7b565b00f222b3706
tortoiseDev <koveeda@gmail.com>
2025-01-05 23:40:51 +0530

    Sync with SPI

--- src/main/java/com/wfa/parser/plugin/SampleParserPlugin.java
+++ src/main/java/com/wfa/parser/plugin/SampleParserPlugin.java
@@ -1,7 +1,12 @@
 package com.wfa.parser.plugin;
 
 import com.wfa.parser.spi.ParserPlugin;
+import com.wfa.parser.spi.api.IComponent;
+import com.wfa.parser.spi.impl.ComponentBean;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
@@ -11,8 +16,9 @@ import com.wfa.middleware.utils.Pair;
 import com.wfa.parser.spi.IParserPlugin;
 
 @Component
-@ParserPlugin(getComponentType = "Sample", getVersion = "0.0.1-SNAPSHOT")
+@ParserPlugin(getId = "SamplePlugin", getComponentTypes = {"Sample", "Smpl"}, getVersion = "0.0.1-SNAPSHOT")
 public class SampleParserPlugin implements IParserPlugin {
+	private static final String STARTS_WITH_PREFIX = "^";
 
 	@Override
 	public Map<String, Pair<Object, DataType>> TokenizeLine() {
@@ -20,12 +26,20 @@ public class SampleParserPlugin implements IParserPlugin {
 	}
 
 	@Override
-	public String getComponentName() {
-		return "Sample";
+	public String[] getFileNameRegexes() {
+		List<String> regexCollection = new ArrayList<String>();
+		for (int i = 0; i < getComponents().length ; i++) {
+			regexCollection.add(STARTS_WITH_PREFIX + getComponents()[i].getComponentName());
+		}
+		
+		return regexCollection.stream().toArray(String[]::new);
 	}
 
 	@Override
-	public String getComponentSource() {
-		return "Sample";
+	public IComponent[] getComponents() {
+		IComponent[] comps = new IComponent[2];
+		comps[0] = new ComponentBean("Sample", "Sample", "Sample");
+		comps[1] = new ComponentBean("Smple", "Smple", "Smple");
+		return comps;
 	}
 }
