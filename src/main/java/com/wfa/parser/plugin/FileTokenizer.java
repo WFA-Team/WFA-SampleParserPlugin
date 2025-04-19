65a4eb01b6b9d53cfc78087ad81ad940ce73c813
tortoiseDev <koveeda@gmail.com>
2025-04-19 20:31:23 +0530

    Changes according to new spi

---
+++ src/main/java/com/wfa/parser/plugin/FileTokenizer.java
@@ -0,0 +1,46 @@
+package com.wfa.parser.plugin;
+
+import java.util.Map;
+
+import com.wfa.middleware.utils.DataType;
+import com.wfa.middleware.utils.Pair;
+import com.wfa.parser.spi.IFileTokenizer;
+
+public class FileTokenizer implements IFileTokenizer{
+
+	private static final String HEADER = "Sample Parser Plugin Logs";
+	private static final String PREFIX = "Sample";
+	private static final String COMMA = ",";
+	
+	@Override
+	public boolean tokenizeLine(String line, Map<String, Pair<Object, DataType>> tokenizedLine) {
+		if (line.contains(HEADER)) 
+			return true; // return true but no publish
+		else if (line.startsWith(PREFIX)) {
+			String kv = line.substring(PREFIX.length()).strip();
+			String[] keyValueArr = kv.split(COMMA);
+			if (keyValueArr.length % 2 != 0) {
+				return false;
+			}
+			
+			for (int i = 0; i < keyValueArr.length; i = i + 2) {
+				String key = keyValueArr[i].strip();
+				String value = keyValueArr[i + 1].strip();
+				
+				try {
+					int intVal = Integer.parseInt(value);
+					tokenizedLine.put(key, new Pair<Object, DataType>(intVal, DataType.INTEGER));
+				} catch(NumberFormatException e1) {
+					try {
+						double realVal = Double.parseDouble(value);
+						tokenizedLine.put(key, new Pair<Object, DataType>(realVal, DataType.REAL));
+					} catch(NumberFormatException e2) {
+						tokenizedLine.put(key, new Pair<Object, DataType>(value, DataType.STRING));
+					}
+				}
+			}	
+			return true;
+		}
+		return false;	
+	}
+}
--- src/main/java/com/wfa/parser/plugin/SampleParserPlugin.java
+++ src/main/java/com/wfa/parser/plugin/SampleParserPlugin.java
@@ -5,26 +5,32 @@ import com.wfa.parser.spi.api.IComponent;
 import com.wfa.parser.spi.impl.ComponentBean;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.wfa.middleware.utils.DataType;
-import com.wfa.middleware.utils.Pair;
+import com.wfa.middleware.utils.beans.data.impl.FileMeta;
+import com.wfa.parser.api.IParserEngine;
+import com.wfa.parser.spi.IFileCompatibilityEvaluator;
+import com.wfa.parser.spi.IFileTokenizer;
+import com.wfa.parser.spi.IFileVisitorStub;
 import com.wfa.parser.spi.IParserPlugin;
 
 @Component
 @ParserPlugin(getId = "SamplePlugin", getComponentTypes = {"Sample", "Smpl"}, getVersion = "0.0.1-SNAPSHOT")
 public class SampleParserPlugin implements IParserPlugin {
 	private static final String STARTS_WITH_PREFIX = "^";
+	private static final String HEADER = "Sample Parser Plugin Logs";
+	private boolean canParse = false;
+	private final IParserEngine engine;
+	private final IFileTokenizer fileTokenizer;
 
-	@Override
-	public Map<String, Pair<Object, DataType>> TokenizeLine() {
-		return new HashMap<String, Pair<Object, DataType>>();
+	@Autowired
+	public SampleParserPlugin(IParserEngine engine) {
+		this.engine = engine;
+		this.fileTokenizer = new FileTokenizer();
 	}
-
+	
 	@Override
 	public String[] getFileNameRegexes() {
 		List<String> regexCollection = new ArrayList<String>();
@@ -41,5 +47,33 @@ public class SampleParserPlugin implements IParserPlugin {
 		comps[0] = new ComponentBean("Sample", "Sample", "Sample");
 		comps[1] = new ComponentBean("Smple", "Smple", "Smple");
 		return comps;
+	}
+
+	@Override
+	public IFileCompatibilityEvaluator getFileCompatibilityEvaluator() {
+		return new IFileCompatibilityEvaluator() {
+
+			@Override
+			public boolean canParse(FileMeta file) {
+				engine.visitFile(file, new IFileVisitorStub() {
+
+					@Override
+					public boolean visitLine(String line) {
+						if (line.contains(HEADER)) {
+							canParse = true;
+							return false;
+						}
+						return true;
+					}
+				}, null);
+				
+				return canParse;
+			}
+		};
+	}
+
+	@Override
+	public IFileTokenizer getFileTokenizer() {
+		return this.fileTokenizer;
 	}
 }
