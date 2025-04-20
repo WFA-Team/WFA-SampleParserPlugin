dd78b2556d66f2cb308fecff0ce03c69c7cb69bf
tortoiseDev <koveeda@gmail.com>
2025-04-20 23:09:33 +0530

    fixes

--- src/main/java/com/wfa/parser/plugin/FileTokenizer.java
+++ src/main/java/com/wfa/parser/plugin/FileTokenizer.java
@@ -2,6 +2,7 @@ package com.wfa.parser.plugin;
 
 import java.util.Map;
 
+import com.wfa.crosstalk.core.model.Constants;
 import com.wfa.middleware.utils.DataType;
 import com.wfa.middleware.utils.Pair;
 import com.wfa.parser.spi.IFileTokenizer;
@@ -9,7 +10,8 @@ import com.wfa.parser.spi.IFileTokenizer;
 public class FileTokenizer implements IFileTokenizer{
 
 	private static final String HEADER = "Sample Parser Plugin Logs";
-	private static final String PREFIX = "Sample";
+	private static final String PREFIX = "Sample:";
+	private static final String TOPIC = "Sample";
 	private static final String COMMA = ",";
 	
 	@Override
@@ -22,6 +24,8 @@ public class FileTokenizer implements IFileTokenizer{
 			if (keyValueArr.length % 2 != 0) {
 				return false;
 			}
+			
+			tokenizedLine.put(Constants.TOPIC, new Pair<Object, DataType>(TOPIC, DataType.STRING));
 			
 			for (int i = 0; i < keyValueArr.length; i = i + 2) {
 				String key = keyValueArr[i].strip();
